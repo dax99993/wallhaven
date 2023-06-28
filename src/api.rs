ab070bc07184483324417a0e1e2ffe0b6b06d8de
Dax99993 <dax99993@gmail.com>
2023-06-28 14:27:55 -0600

    Added some structs to have an custom API Handler

--- src/api.rs
+++ src/api.rs
@@ -34,3 +34,111 @@ pub fn save_key(path: &str, api: &str) -> Result<(), String> {
 
    Ok(())
 }
+
+
+
+struct APIBuilder {
+    query: Vec<Query>,
+    categories: Vec<Category>,
+    purity: Vec<Purity>,
+    sorting: Sorting,
+    order: Order,
+    toprange: TopRange, //sorting must be TopList to apply this
+    atleast: Option<Resolution>,
+    resolutions: Vec<Resolution>,
+    ratios: Vec<Resolution>, //maybe create type Ratio
+    colors: Vec<HexColor>,
+    page: usize, //should start at 1
+    seed: AlphaNumericalSeed,
+}
+
+enum Query {
+    Tags(Vec<Tag>),
+    Username(String),
+    Id(i32), //Cant be combined
+    Type(), //type:{png/jpg}
+    Like(String), 
+}
+
+enum Tag {
+    Include(String),
+    Exclude(String),
+}
+
+use std::convert::AsRef;
+use strum::AsRefStr;
+
+#[derive(Debug, AsRefStr, Default, PartialEq)]
+//#[derive(Debug, Default)]
+enum Sorting {
+    #[default]
+    #[strum(serialize = "date_added")]
+    DateAdded,
+    Relevance,
+    Random,
+    Views,
+    Favorites,
+    Toplist
+}
+
+#[derive(Default)]
+enum Order {
+    #[default]
+    Desc,
+    Asc,
+}
+
+#[derive(Default)]
+enum TopRange {
+    OneDay,
+    ThreeDays,
+    OneWeek,
+    #[default]
+    OneMonth,
+    ThreeMonths,
+    SixMonths,
+    OneYear,
+}
+
+#[derive(Default)]
+enum Category {
+    #[default]
+    General,
+    Anime,
+    People,
+}
+
+#[derive(Debug, Default, PartialEq)]
+enum Purity{
+    #[default]
+    Sfw,
+    Sketchy,
+    Nsfw,
+}
+
+struct Resolution(usize, usize);
+
+struct HexColor(String);
+
+struct AlphaNumericalSeed(String);
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use crate::api::{Purity, Sorting};
+
+    #[test]
+    fn it_works() {
+        let p = Purity::default();
+        println!("{:?}", p);
+        assert_eq!(p, Purity::Sfw);
+    }
+    #[test]
+    fn sorting_as_str() {
+        let s = Sorting::default();
+        println!("{:?}", s.as_ref());
+        assert_eq!(s.as_ref(), "date_added");
+    }
+}
